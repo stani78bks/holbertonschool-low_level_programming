94e10034528cf47f70bf081190e664c252f1f3ef
stani78bks <10648@holbertonstudents.com>
2025-02-14 00:50:24 +0100

    ee

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+/**
+*main - This uses program prints exactly the string
+*Programming is like building a multilingual puzzle
+*followed by a new line.
+*Return: always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
