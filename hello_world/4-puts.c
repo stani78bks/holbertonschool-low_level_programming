87d18a855c98a8eb37593b4f9121b8e2877a2c0f
stani78bks <10648@holbertonstudents.com>
2025-02-14 00:46:41 +0100

    tt

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - is the entry point of a C program.
+*It's where the program starts executing.
+*When you run a C program, the operating system calls the main function first.
+*Everything inside the main function is executed step by step.
+*It's like the starting line of a race for your code
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
