566baf319b0ee6e94ba3308f2974026818394341
stani78bks <10648@holbertonstudents.com>
2025-02-14 01:30:49 +0100

    re

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char lettre;
+for (lettre = 'a' ; lettre <= 'z' ; lettre++)
+{
+putchar(lettre);
+}
+for (lettre = 'A' ; lettre <= 'Z' ; lettre++)
+{
+putchar(lettre);
+}
+putchar('\n');
+return (0);
+}
