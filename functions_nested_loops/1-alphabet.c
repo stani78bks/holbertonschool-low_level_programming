02e4dcaf5a173908f0a04a6cf52117c143c95533
stani78bks <10648@holbertonstudents.com>
2025-02-14 12:33:25 +0100

    rr

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char lettre = 'a';
+while (lettre <= 'z')
+{
+_putchar(lettre);
+lettre++;
+}
+_putchar('\n');
+}
