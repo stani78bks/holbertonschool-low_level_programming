d9fbb3908bcbe7b2ea01183b663d33cbac46214e
stani78bks <10648@holbertonstudents.com>
2025-02-14 12:20:32 +0100

    rr

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _putchar(char c);
+
+#endif /* MAIN_H */
