50d1d2272d72f6b94256f2b5fb1ad090979239fb
stani78bks <10648@holbertonstudents.com>
2025-02-24 12:33:32 +0100

    ee

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - rints the numbers, from 0 to 9
+ *
+ * Return: Always 0 (Sucess)
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
