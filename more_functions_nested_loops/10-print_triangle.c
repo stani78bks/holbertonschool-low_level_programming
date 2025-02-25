985fa99d797087c4d847b88dcc48f9801a149062
stani78bks <10648@holbertonstudents.com>
2025-02-25 18:44:18 +0100

    ee

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
  */
 void print_char(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
 /**
@@ -16,30 +16,30 @@ void print_char(char c)
  */
 void print_triangle(int size)
 {
-    int row, spaces, hashes;
+	int row, spaces, hashes;
 
-    if (size <= 0)
-    {
-        print_char('\n'); /* Affiche une ligne vide */
-        return;
-    }
+	if (size <= 0)
+	{
+		print_char('\n'); /* Affiche une ligne vide */
+		return;
+	}
 
-    for (row = 1; row <= size; row++)
-    {
-        /* Afficher les espaces pour aligner le triangle */
-        for (spaces = 1; spaces <= size - row; spaces++)
-        {
-            print_char(' ');  /* Utilisation d'un espace au lieu d'un point */
-        }
+	for (row = 1; row <= size; row++)
+	{
+		/* Afficher les espaces pour aligner le triangle */
+		for (spaces = 1; spaces <= size - row; spaces++)
+		{
+			print_char(' ');
+		}
 
-        /* Afficher les dièses */
-        for (hashes = 1; hashes <= row; hashes++)
-        {
-            print_char('#');
-        }
+		/* Afficher les dièses */
+		for (hashes = 1; hashes <= row; hashes++)
+		{
+			print_char('#');
+		}
 
-        /* Aller à la ligne */
-        print_char('\n');
-    }
+		/* Aller à la ligne */
+		print_char('\n');
+	}
 }
 
