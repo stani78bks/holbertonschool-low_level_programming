ea526b0db305193e04404cda767352b2a57179d0
stani78bks <10648@holbertonstudents.com>
2025-02-25 14:24:40 +0100

    rr

---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Écrit un caractère sur la sortie standard.
+ * @c: Le caractère à afficher.
+ *
+ * Return: 1 en cas de succès, -1 en cas d'erreur.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
