5446122da084efcaca01bf34e21180ca128d61df
stani78bks <10648@holbertonstudents.com>
2025-02-25 15:57:10 +0100

    ee

--- more_functions_nested_loops/10-main.c
+++ more_functions_nested_loops/10-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - Fonction principale pour tester print_triangle.
+ * main - check the code
  *
- * Return: Toujours 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-    print_triangle(5);  // Tester avec un triangle de taille 5.
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
     return (0);
 }
-
