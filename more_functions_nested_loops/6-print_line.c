0341e302b582a833c6a83b688d1c137555212762
stani78bks <10648@holbertonstudents.com>
2025-02-25 14:18:42 +0100

    ee

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -4,7 +4,7 @@
  * print_line - Draws a straight line in the terminal.
  * @n: The number of times the character '_' should be printed.
  *
- * Return: void
+ * Description: If n is 0 or less, only a newline is printed.
  */
 void print_line(int n)
 {
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-int _putchar (char c);
-void print_most_numbers(void);
-void more_numbers(void);
+int _putchar(char c);
+void print_line(int n);
 
-#endif
+#endif /* MAIN_H */
+
