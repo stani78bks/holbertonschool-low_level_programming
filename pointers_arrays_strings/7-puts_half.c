7f546e4a6fda5a896680e5ca7d0186db83da4c36
stani78bks <10648@holbertonstudents.com>
2025-02-26 23:12:17 +0100

    ee

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * puts_half - Prints the second half of a string
@@ -7,18 +6,19 @@
  */
 void puts_half(char *str)
 {
-    int length = 0, n, i;
+	int length = 0, n, i;
 
-    /* Trouver la longueur de la chaîne */
-    while (str[length] != '\0')
-        length++;
+	/* Trouver la longueur de la chaîne */
+	while (str[length] != '\0')
+		length++;
 
-    /* Calculer le point de départ pour l'affichage */
-    n = (length + 1) / 2;
+	/* Calculer le point de départ pour l'affichage */
+	n = (length + 1) / 2;
 
-    /* Afficher la seconde moitié */
-    for (i = n; i < length; i++)
-        _putchar(str[i]);
+	/* Afficher la seconde moitié */
+	for (i = n; i < length; i++)
+		_putchar(str[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
+
