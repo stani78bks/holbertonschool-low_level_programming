5e818f1ebe20e0cb68c9e9efa79940840cd4401c
stani78bks <10648@holbertonstudents.com>
2025-02-26 22:53:20 +0100

    rr

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * rev_string - Inverse une chaîne de caractères.
- * @s: Pointeur vers la chaîne de caractères.
+ * rev_string - Reverses a string
+ * @s: Pointer to the string
  */
 void rev_string(char *s)
 {
-    int len = 0, i;
-    char temp;
+	int len = 0, i;
+	char temp;
 
-    /* Trouver la longueur de la chaîne */
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	/* Find the length of the string */
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    /* Inverser la chaîne de caractères */
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    }
+	/* Reverse the string */
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
+
