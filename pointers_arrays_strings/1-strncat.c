d86fe42944c9e798c34cad59ef4b181dea00479d
stani78bks <10648@holbertonstudents.com>
2025-02-27 00:47:51 +0100

    ee

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ * @n: Maximum number of bytes to use from src
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int i = 0, j = 0;
+
+    /* Trouver la fin de la chaîne dest */
+    while (dest[i] != '\0')
+        i++;
+
+    /* Ajouter src à dest en utilisant au plus n caractères */
+    while (src[j] != '\0' && j < n)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    /* Ajouter le caractère nul de fin */
+    dest[i] = '\0';
+
+    return (dest);
+}
