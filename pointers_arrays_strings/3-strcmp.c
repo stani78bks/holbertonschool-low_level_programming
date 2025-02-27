eed29d64b957f0b6e9d2d850b8469e71102afc24
stani78bks <10648@holbertonstudents.com>
2025-02-27 01:06:06 +0100

    ee

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare deux chaînes de caractères.
+ * @s1: La première chaîne à comparer.
+ * @s2: La deuxième chaîne à comparer.
+ *
+ * Return: Un nombre négatif si s1 est inférieure à s2, 
+ *         un nombre positif si s1 est supérieure à s2, 
+ *         0 si les deux chaînes sont égales.
+ */
+int _strcmp(char *s1, char *s2)
+{
+    while (*s1 && *s2)
+    {
+        if (*s1 != *s2)
+        {
+            return (*s1 - *s2);  /* Retourne la différence entre les deux caractères */
+        }
+        s1++;
+        s2++;
+    }
+
+    return (*s1 - *s2);  /* Si les chaînes sont égales jusqu'à la fin de l'une d'elles */
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Déclaration de la fonction _strncpy */
-char *_strncpy(char *dest, char *src, int n);
+/* Déclaration de la fonction _strcmp */
+int _strcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
 
