2e5ee99838c9575515642b6f4433c1040ea5da8a
stani78bks <10648@holbertonstudents.com>
2025-03-03 23:03:37 +0100

    ee

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,13 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *str);
-char *cap_string(char *str);
-char *leet(char *str);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif /* MAIN_H */
 
