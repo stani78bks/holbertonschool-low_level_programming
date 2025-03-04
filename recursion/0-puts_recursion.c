bf52339eba588a9b1fbbac6d623745e3bac35bb0
stani78bks <10648@holbertonstudents.com>
2025-03-04 15:44:20 +0100

    rr

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - imprime une chaîne suivie d'un saut de ligne
- * @s: la chaîne à imprimer
+ * _puts_recursion - Affiche une chaîne de caractères suivie d'un saut de ligne.
+ * @s: La chaîne de caractères à afficher.
  */
 void _puts_recursion(char *s)
 {
-    if (*s)  /* Cas de base : si la chaîne n'est pas vide */
+    if (*s)  /* Si on n'a pas atteint la fin de la chaîne */
     {
-        _putchar(*s);  /* Imprime le caractère */
+        _putchar(*s);  /* Affiche le caractère courant */
         _puts_recursion(s + 1);  /* Appel récursif pour le caractère suivant */
     }
     else
     {
-        _putchar('\n');  /* Imprime un saut de ligne lorsque la chaîne est entièrement imprimée */
+        _putchar('\n');  /* Affiche un saut de ligne une fois la chaîne terminée */
     }
 }
+
