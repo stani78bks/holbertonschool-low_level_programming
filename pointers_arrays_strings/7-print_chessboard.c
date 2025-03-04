6b507d064e9d09dd3856ebbc9c1b5f394f2a9557
stani78bks <10648@holbertonstudents.com>
2025-03-04 12:39:17 +0100

    Correction print_chessboard: respect des normes Betty et ajout de _putchar

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_chessboard - Affiche un échiquier de 8x8 caractères.
- * @a: Tableau 2D contenant les pièces de l'échiquier.
+ * print_chessboard - Affiche un échiquier 8x8.
+ * @a: Tableau 2D contenant l'échiquier.
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 8; i++) /* Boucle à travers les lignes */
-    {
-        for (j = 0; j < 8; j++) /* Boucle à travers les colonnes */
-        {
-            _putchar(a[i][j]); /* Affiche le caractère */
-        }
-        _putchar('\n'); /* Nouvelle ligne après chaque ligne de l'échiquier */
-    }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]); /* Affiche chaque caractère */
+		}
+		_putchar('\n'); /* Passe à la ligne après chaque ligne de l'échiquier */
+	}
 }
+
