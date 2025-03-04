5316a5c5a397d945404d6352b80b0f830fa48bc3
stani78bks <10648@holbertonstudents.com>
2025-03-04 15:57:45 +0100

    rr

--- recursion/_putchar.c
+++ recursion/_putchar.c
@@ -1,12 +1,13 @@
-#include <unistd.h>
+#include <unistd.h>  /* For the write system call */
 
 /**
- * _putchar - imprime un caractère à stdout
- * @c: Le caractère à imprimer
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Retour : 1 en cas de succès, -1 en cas d'erreur
+ * Return: On success, 1 is returned. On error, -1 is returned.
  */
 int _putchar(char c)
 {
-    return write(1, &c, 1);  /* Utilise l'appel système write pour imprimer un caractère */
+    return (write(1, &c, 1));  /* Use write system call to print a character */
 }
+
