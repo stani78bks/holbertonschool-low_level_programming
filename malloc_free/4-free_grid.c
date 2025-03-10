303eca0efbc9f689964f6899dbf46e2f74873557
stani78bks <10648@holbertonstudents.com>
2025-03-11 00:37:38 +0100

    ee

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Libère un tableau 2D alloué avec alloc_grid.
+ * @grid: Le tableau 2D à libérer.
+ * @height: La hauteur du tableau.
+ *
+ * Retour: Rien.
+ */
+void free_grid(int **grid, int height)
+{
+    int i;
+
+    if (grid == NULL)
+        return;
+
+    /* Libérer chaque ligne */
+    for (i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+
+    /* Libérer le tableau principal */
+    free(grid);
+}
+
