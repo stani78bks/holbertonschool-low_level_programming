3dd1308d0e327d0751a69f0e22bdd958416c23bf
stani78bks <10648@holbertonstudents.com>
2025-03-11 00:51:44 +0100

    e

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -9,11 +9,13 @@
  */
 void free_grid(int **grid, int height)
 {
+	int i;  /* Déclaration de i avant la boucle */
+
 	if (grid == NULL)
 		return;
 
 	/* Libérer chaque ligne */
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
