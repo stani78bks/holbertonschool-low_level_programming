39f4946f9972302ce646be4a7ff0bdb49a30876a
Nada <Nada@MacBook-Pro-de-Macbook.local>
2025-04-02 12:52:17 +0200

    task0 a 5

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: Nothing.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
